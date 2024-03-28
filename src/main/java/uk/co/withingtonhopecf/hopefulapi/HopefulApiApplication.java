27445b57710aaba554e2095e18fde95d8b76aa27
connor-kiernan <connorkierna@outlook.com>
2024-03-28 17:15:08 +0000

    initial commit

---
+++ src/main/java/uk/co/withingtonhopecf/hopefulapi/HopefulApiApplication.java
@@ -0,0 +1,13 @@
+package uk.co.withingtonhopecf.hopefulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopefulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopefulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/co/withingtonhopecf/hopefulapi/HopefulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.withingtonhopecf.hopefulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopefulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
