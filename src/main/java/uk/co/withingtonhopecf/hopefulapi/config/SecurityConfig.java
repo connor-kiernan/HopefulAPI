a914b0d4eaad79cdd7c2dd27d62dace1c3b9f201
connor-kiernan <connorkierna@outlook.com>
2024-04-08 18:27:05 +0100

    Add auth handling

--- src/main/java/uk/co/withingtonhopecf/hopefulapi/config/SecurityConfig.java
+++ src/main/java/uk/co/withingtonhopecf/hopefulapi/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package uk.co.withingtonhopecf.hopefulapi.config;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 
@@ -22,17 +26,35 @@ public class SecurityConfig {
 			.cors(corsConfigurer -> corsConfigurer.configurationSource(request -> {
 				CorsConfiguration configuration = new CorsConfiguration();
 				configuration.setAllowedOrigins(List.of(config.frontendUrl()));
-				configuration.setAllowedMethods(List.of("GET"));
+				configuration.setAllowedMethods(List.of("GET", "POST"));
+				configuration.setAllowCredentials(true);
+				configuration.setAllowedHeaders(List.of("*"));
 
 				return configuration;
 			}))
 			.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
 				authorizationManagerRequestMatcherRegistry
+					.requestMatchers("/auth/*").permitAll()
 					.requestMatchers("/players").permitAll()
 					.requestMatchers("/matches").permitAll()
 					.anyRequest().authenticated()
-			).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+			).oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(grantedAuthoritiesExtractor())))
+			.sessionManagement(sessionManagementConfigurer -> sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.build();
+	}
+
+	private JwtAuthenticationConverter grantedAuthoritiesExtractor() {
+		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+			List<String> l = jwt.getClaimAsStringList("cognito:groups");
+
+			return l.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toCollection(ArrayList::new));
+		});
+
+		jwtAuthenticationConverter.setPrincipalClaimName("cognito:username");
+
+		return jwtAuthenticationConverter;
 	}
 
 }
